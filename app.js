095cf8dfb92fc29b7fed42fda4124cec1d68e5e3
Asfiya hussain <126233785+asfiyahussain00@users.noreply.github.com>
2024-09-23 17:07:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+
+//  primitive data-types in JavaScript
+
+
+// primitive data-types in number
+
+// let num = 34;
+//  let num1 = 90.45;
+
+// console.log("value of num" ,  num );
+// console.log("value of num" ,  num1);
+
+// To check the data type 
+// console.log( typeof(num1));
+
+
+
+// primitive data-types in  string
+ 
+// let name = "asfi";
+// let name1 = "hafsa";
+
+// console.log("value of num" ,  name );
+// console.log("value of num" ,  name1);
+
+
+
+
+// primitive data-types in  undefined
+// let x ;
+// console.log(x);
+
+
+
+// primitive data-types in  null
+// let y  = null;
+// console.log(y);
+
+
+// primitive data-types in  Boolean
+
+// let isclick = (false)
+//  console.log(isclick)
+// // To check the data type 
+// console.log( typeof(isclick));
+
+
+
+// primitive data-types in  BigInit
+
+// let bigNum = 123422222222222222222222222222222222222n
+// console.log(bigNum)
+
+
+
+// primitive data-types in  symbols
+
+// let sym = Symbol("Hello");
+// console.log(sym);
+
+
+// Non-primitive Data Types
+
+// const student = {  
+//       stdName  : "Asfiya" ,
+//       FName  : "Hussain" ,
+//       RollNo : 2345 ,
+//       Coruse : "Web development"};
+
+// console.log(student);
+
+// //  To change any key value 
+//   student["stdName"] = "hafsa";
+//   console.log(student["stdName"]);
+
+
+
+
+//  This is use for singal line Comment
+/*  This is USE  for multiple line line Comment*/
+
+
+
+// Operator in Js
+
+// let num1 = 5;
+// let num2 = 5 ;
+// console.log( "Increment" , num1+num2)
+// console.log( "decrement" , num1-num2)
+// console.log("Exponentiation" , num1*num2)
+// console.log( "modules" , num1/num2)
+
+
+
+// use for new line
+// alert("happy coding!\n prevent this page from creating additional dialogs");
+
+//  in documte.write we use <br> for new line
+// document.write("Happy coding <br> hello javascript")
+
+
